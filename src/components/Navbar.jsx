e748c46749732c0fadef26d19b5216d48c44fcd3
Giovanni Tramontano <giovanni.tramontano.gt@gmail.com>
2025-08-06 15:18:11 +0200

    Corretto comportamento al click dropdown navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import Flag from "react-world-flags"
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import i18next from "i18next";
 
 import LangContext from "./context/LangContext";
@@ -9,13 +9,25 @@ import GooeyNav from "./reactbits-components/GooeyNav";
 function Navbar (){
     const { lang, setLang } = useContext(LangContext);
     const { t } = useTranslation();
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
     const changeLanguage = (code) => {
         setLang(code);
         i18next.changeLanguage(code, (err, t) => {
             if (err) return console.log('something went wrong loading', err);
             t(code);
+            setDropdownOpen(false);
         });
     }
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
     const items = [
         {
             label: t("navbar_home"),
@@ -46,12 +58,16 @@ function Navbar (){
                 timeVariance={300}
                 colors={[1, 2, 3, 1, 2, 3, 1, 4]}
             />
-            <div className="dropdown dropdown-center dropdown-bottom cursor-pointer">
-                <div tabIndex={0} role="button" className="flex items-center gap-1 uppercase"><Flag code={lang} height="30" width="24"/> <span className="hidden lg:inline">{lang}</span></div>
-                <ul tabIndex={0} className="dropdown-content menu bg-white/10 backdrop-blur rounded-box z-1 w-35 p-2 shadow-sm">
-                    <li className="lg:text-base text-xs" onClick={() => changeLanguage("it")}><a><Flag code="IT" height="30" width="24"/> IT</a></li>
-                    <li className="lg:text-base text-xs" onClick={() => changeLanguage("us")}><a><Flag code="US" height="30" width="24"/> US</a></li>
+            <div ref={dropdownRef} className="relative cursor-pointer">
+                <div tabIndex={0} role="button" className="flex items-center gap-1 uppercase" onClick={() => setDropdownOpen(!isDropdownOpen)}>
+                    <Flag code={lang} height="30" width="24"/> <span className="hidden lg:inline">{lang}</span>
+                </div>
+                {isDropdownOpen && 
+                <ul tabIndex={0} className="absolute mt-2 bg-white/10 backdrop-blur rounded-box z-1 w-35 p-2 shadow-sm">
+                    <li className="lg:text-base text-xs" onClick={() => changeLanguage("it")}><a className="flex items-center gap-2 hover:bg-white/20 transition duration-300 p-1"><Flag code="IT" height="30" width="24"/> IT</a></li>
+                    <li className="lg:text-base text-xs" onClick={() => changeLanguage("us")}><a className="flex items-center gap-2 hover:bg-white/20 transition duration-300 p-1"><Flag code="US" height="30" width="24"/> US</a></li>
                 </ul>
+                } 
             </div>
         </nav>
     )
